fa365fdfde8709cf38b04ead512df2efee63fb1f
Sachin007060 <sachinbaskandi20@gmail.com>
2024-04-20 22:05:52 +0530

    add program no 77 of java file

---
+++ ProgramNo77.java
@@ -0,0 +1,7 @@
+public class ProgramNo77 {
+    public static void main(String[] args) {
+        double arr[] = { 1.2, 2.3, 4.5, 23.01, 23.8, 12.8, 3.4, 6.2, 45.7, 94.4 };
+        for (int i = 0; i < 10; i++)
+            System.out.print(arr[i] + "  ");
+    }
+}
